3adacd4508edb3198bc706a83f796fa46800c637
Jeff Knafo <jeff.knafo@edu.devinci.fr>
2023-10-11 09:04:47 +0200

    Init Project

---
+++ ex1.js
@@ -0,0 +1,12 @@
+//Exercice 1
+
+class student{
+    constructor(name, attack, defense, hp, luck) {
+        this.name = name
+        this.attack = attack
+        this.defense = defense
+        this.hp = hp
+        this.luck = luck
+    }
+}
+
